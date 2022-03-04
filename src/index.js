b957c1705d7c3e898bc5e562b351d12328c63518
Cuong Dong Minh <mcd.oblivion@gmail.com>
2022-03-05 00:26:47 +0700

    Update Apollo URI

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
-});
+    uri: 'https://lift-off-catstronauts-cuongdm.herokuapp.com/',
+    cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
